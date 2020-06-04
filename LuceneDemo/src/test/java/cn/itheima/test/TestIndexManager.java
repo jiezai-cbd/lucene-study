3d42f123a644d976ba81c45e326741de40b2c39a
jiezai-cbd <wwjpcx@163.com>
2020-06-04 16:35:01 +0800

    #

--- LuceneDemo/src/test/java/cn/itheima/test/TestIndexManager.java
+++ LuceneDemo/src/test/java/cn/itheima/test/TestIndexManager.java
@@ -8,6 +8,7 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
@@ -69,34 +70,27 @@ public class TestIndexManager {
     }
 
 
-
+    /**
+     *
+     * @throws Exception
+     */
     @Test
     public void updateIndexTest() throws Exception{
-        //1.采集数据
-        //1. 采集数据
-        SkuDao skuDao = new SkuDaoImpl();
-        List<Sku> skuList = skuDao.querySkuList();
 
-        //文档集合
-        List<Document> docList = new ArrayList<>();
 
-        for (Sku sku : skuList) {
-            //2. 创建文档对象
             Document document = new Document();
-            document.add(new StringField("id", sku.getId(), Field.Store.YES));
-            document.add(new TextField("name", sku.getName(), Field.Store.YES));
+            document.add(new StringField("id", "100000003145", Field.Store.YES));
+            document.add(new TextField("name", "xxx", Field.Store.YES));
             //分词加索引
-            document.add(new IntPoint("price", sku.getPrice()));
+            document.add(new IntPoint("price", 123));
             //存储
-            document.add(new StoredField("price", sku.getPrice()));
+            document.add(new StoredField("price", 123));
             //document.add(new TextField("price", sku.getPrice(), Field.Store.YES));
-            document.add(new StoredField("image", sku.getImage()));
-            document.add(new StringField("categoryName", sku.getCategoryName(), Field.Store.YES));
-            document.add(new StringField("brandName", sku.getBrandName(), Field.Store.YES));
+            document.add(new StoredField("image", "xxxx.jpg"));
+            document.add(new StringField("categoryName", "手机", Field.Store.YES));
+            document.add(new StringField("brandName", "华为", Field.Store.YES));
 
-            //将文档对象放入到文档集合中
-            docList.add(document);
-        }
+
         //3. 创建分词器, StandardAnalyzer标准分词器, 对英文分词效果好, 对中文是单字分词, 也就是一个字就认为是一个词.
         Analyzer analyzer = new StandardAnalyzer();
         //4. 创建Directory目录对象, 目录对象表示索引库的位置
@@ -105,13 +99,35 @@ public class TestIndexManager {
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         //6. 创建IndexWriter输出流对象, 指定输出的位置和使用的config初始化对象
         IndexWriter indexWriter = new IndexWriter(dir, config);
-        //7. 写入文档到索引库
-        for (Document doc : docList) {
-            indexWriter.addDocument(doc);
-        }
+
+        //修改
+       indexWriter.updateDocument(new Term("id","100000003145"),document);
+
         //8. 释放资源
         indexWriter.close();
     }
 
+
+    @Test
+    public void deleteIndexTest() throws Exception{
+
+
+
+        //3. 创建分词器, StandardAnalyzer标准分词器, 对英文分词效果好, 对中文是单字分词, 也就是一个字就认为是一个词.
+        Analyzer analyzer = new StandardAnalyzer();
+        //4. 创建Directory目录对象, 目录对象表示索引库的位置
+        Directory dir = FSDirectory.open(Paths.get("E:\\dir"));
+        //5. 创建IndexWriterConfig对象, 这个对象中指定切分词使用的分词器
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        //6. 创建IndexWriter输出流对象, 指定输出的位置和使用的config初始化对象
+        IndexWriter indexWriter = new IndexWriter(dir, config);
+
+        //根据条件删除
+        //indexWriter.deleteDocuments(new Term("id","100000003145"));
+        //删除所有
+        indexWriter.deleteAll();
+        //8. 释放资源
+        indexWriter.close();
+    }
 }
 
